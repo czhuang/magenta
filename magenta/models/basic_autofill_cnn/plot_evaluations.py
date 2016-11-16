4734c73811019a636fdb3607cd2fc554ab36df16
Anna Huang <chengzhiannahuang@gmail.com>
2016-11-16 17:59:19 -0500

    add nade line

--- magenta/models/basic_autofill_cnn/plot_evaluations.py
+++ magenta/models/basic_autofill_cnn/plot_evaluations.py
@@ -5,6 +5,13 @@ from datetime import datetime
 import numpy as np
 import pylab as plt
 
+#from magenta.models.basic_autofill_cnn import evaluation_tools
+#from magenta.models.basic_autofill_cnn import retrieve_model_tools
+
+
+TEST_MODE = False
+NOTEWISE = True
+
 
 def get_current_time_as_str():
   return datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
@@ -16,8 +23,21 @@ def get_fpath_wrapper(fname_tag='', file_type='png'):
                        '%s_%s.%s' % (fname_tag, get_current_time_as_str(), file_type))
   return fpath
 
-TEST_MODE = False
-NOTEWISE = True
+
+def get_NADE_nll_mean_and_sem():
+  #base_path_nade = '/Tmp/huangche/compare_sampling/nade_unzip/20161112215554_nade'
+  #pianorolls = np.load(fpaths[name])['pianorolls']
+  #assert pianorolls.shape == (1, 100, 32, 53, 4)
+  #pianoroll = pianorolls[0]
+  #model_name = 'balanced_by_scaling'
+  #wrapped_model = retrieve_model_tools.retrieve_model(model_name=model_name)
+  #wrapped_model.config.hparams.crop_piece_len = 32
+  #if NOTEWISE:
+  #  losses = evaluation_tools.compute_notewise_loss(wrapped_model, pianorolls)  
+  if NOTEWISE:
+    return 0.56509, 0.01071 
+  else:
+    assert False, 'not yet have number for chordwise'
 
 # actual long run
 stats_fpath = '/Tmp/huangche/compare_sampling/stats_2016-11-15_13:56:11.pkl'
@@ -62,9 +82,9 @@ if TEST_MODE:
 #aggregated_sems = np.asarray(aggregated_sems)
 #print aggregated_means.shape, aggregated_sems.shape
 
-legend_names = {'sequential':'Contiguous(4timestep*16, covering50%) (sequential in block)',
+legend_names = {'sequential':'Contiguous(4timestep*16, covering50%)\n (sequential in block)',
 
-                'independent': 'Yao (2014) annealed block size (independent in block)',
+                'independent': 'Yao (2014) annealed block size\n (independent in block)',
                 '50': 'Bernoulli(50) (sequential in block)',
                 '75': 'Bernoulli(75) (sequential in block)',
                 '99': 'Bernoulli(99) (sequentail in block)'}
@@ -73,11 +93,19 @@ plt.figure()
 for method_name in method_names:
   means, sems = aggregated_lls_stats[method_name] 
   plt.errorbar(sorted_iters, means, yerr=sems, label='%s' % legend_names[method_name])
+# Add Nade line.
+nade_mean, nade_sem = get_NADE_nll_mean_and_sem()
+plt.axhline(y=nade_mean, xmin=0, xmax=101, linewidth=1, color = 'k', label='NADE ancestral sampling')
+plt.axhline(y=nade_mean+nade_sem, xmin=0, xmax=101, linewidth=0.5, color = 'k', linestyle='dotted')
+plt.axhline(y=nade_mean-nade_sem, xmin=0, xmax=101, linewidth=0.5, color = 'k', linestyle='dotted')
+
 plt.legend(prop={'size':10})
 plt.xlim(-1, 101)
 plt.xlabel('# of Gibbs steps')
 plt.ylabel('Negative log-likelihood (NLL)')
 plt.title("Note-wise NLL for different Blocked-Gibbs setups")
+
+
 plot_fpath = get_fpath_wrapper('nll_curve-notewise', 'png')
 print 'Saving plot to', plot_fpath
 plt.savefig(plot_fpath)
