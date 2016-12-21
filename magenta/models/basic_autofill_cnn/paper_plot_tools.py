7edb6207d35f2ea89b779814674745dc4984caa3
Anna Huang <chengzhiannahuang@gmail.com>
2016-12-21 13:49:29 -0500

    plot colored masks, retrieve corresponding inpainting task npz, get pitch bounds

--- magenta/models/basic_autofill_cnn/paper_plot_tools.py
+++ magenta/models/basic_autofill_cnn/paper_plot_tools.py
@@ -6,11 +6,11 @@ import numpy as np
 import matplotlib.pylab as plt
 from matplotlib.patches import Rectangle
 from matplotlib import colors
-import tensorflow as tf
+#import tensorflow as tf
 
-FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_string('plot_dir', '/u/huangche/plots/',
-       'Path to the directory where plots will be stored.')
+#FLAGS = tf.app.flags.FLAGS
+#tf.app.flags.DEFINE_string('plot_dir', '/u/huangche/plots/',
+#       'Path to the directory where plots will be stored.')
 
 
 #--------------
@@ -45,7 +45,8 @@ def plot_pianoroll_with_colored_voices_wrapper():
 
 # Uitilities
 def get_unique_output_path():
-  path = FLAGS.plot_dir
+  path = '/u/huangche/plots/'
+  #path = FLAGS.plot_dir
   timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
   output_path = os.path.join(path, timestamp)
   if not os.path.exists(output_path):
@@ -73,17 +74,39 @@ GENERATED_COLORS = np.array([[  4.17642000e-01,   5.64000000e-04,   6.58390000e-
 CONTEXT_COLORS = GENERATED_COLORS
 
 
-def get_npz_data(fpath=None):
-  if fpath is None:
-    # For second main version paper plot on NADE.
-    fpath = '/Tmp/huangche/new_generation/fromscratch_balanced_by_scaling_init=bach_Gibbs-num-steps-0--masker-None--schedule-ConstantSchedule-None---sampler-SequentialSampler-temperature-1e-05--_20161121235937_1.03min.npz'
-    # Later moved to /data/lisatmp4
+def get_fpath(context_kind, sampling_method):
+  # NADE samples.
+  if context_kind == 'transition' and sampling_method == 'nade':
+    # Transition, NADE, second main version paper plot.
     fpath = '/data/lisatmp4/huangche/new_generated/fromscratch_balanced_by_scaling_init=bach_Gibbs-num-steps-0--masker-None--schedule-ConstantSchedule-None---sampler-SequentialSampler-temperature-1e-05--_20161121235937_1.03min.npz'
-    fpath = '/data/lisatmp4/huangche/new_generated/fromscratch_balanced_by_scaling_init=independent_Gibbsnumsteps2maskerBernoulliInpaintingMaskercontextkindharmonizationscheduleYaoSchedulepmin01pmax09alpha07samplerIndependentSamplertemperature1e05_20161218173017_0.03min.npz'
-    fpath = '/data/lisatmp4/huangche/new_generated/fromscratch_balanced_by_scaling_init=independent_Gibbsnumsteps3maskerBernoulliInpaintingMaskercontextkindharmonizationscheduleYaoSchedulepmin01pmax09alpha07samplerIndependentSamplertemperature1e05_20161218205028_0.04min.npz'
-    # 100 steps independent Gibbs.
+  elif context_kind == 'harmonization' and sampling_method == 'nade':
+    fpath = '/data/lisatmp4/huangche/new_generated/fromscratch_balanced_by_scaling_init=sequential_Gibbsnumsteps0maskerBernoulliInpaintingMaskercontextkindharmonizationscheduleConstantScheduleNonesamplerNone_20161220190944_0.00min.npz'
+
+  # YAO samples.  
+  elif context_kind == 'transition' and sampling_method == 'yao':
+    # Transition, 100 steps independent Gibbs.
+#    fpath = '/data/lisatmp4/huangche/new_generated/fromscratch_balanced_by_scaling_init=independent_Gibbsnumsteps100maskerBernoulliInpaintingMaskercontextkindtransitionscheduleYaoSchedulepmin01pmax09alpha07samplerIndependentSamplertemperature1e05_20161220160427_1.41min.npz'
+
+    # Transition, 64 steps of I. gibbs (to use the same number of model evaluations as NADE.
+    fpath = '/data/lisatmp4/huangche/new_generated/fromscratch_balanced_by_scaling_init=independent_Gibbsnumsteps64maskerBernoulliInpaintingMaskercontextkindtransitionscheduleYaoSchedulepmin01pmax09alpha07samplerIndependentSamplertemperature1e05_20161220195113_0.90min.npz'
+
+    # Take 2 of above.
+    fpath = '/data/lisatmp4/huangche/new_generated/fromscratch_balanced_by_scaling_init=independent_Gibbsnumsteps64maskerBernoulliInpaintingMaskercontextkindtransitionscheduleYaoSchedulepmin01pmax09alpha07samplerIndependentSamplertemperature1e05_20161220200126_0.91min.npz'
+
+    # Take 3 of above.
+    fpath = '/data/lisatmp4/huangche/new_generated/fromscratch_balanced_by_scaling_init=independent_Gibbsnumsteps64maskerBernoulliInpaintingMaskercontextkindtransitionscheduleYaoSchedulepmin01pmax09alpha07samplerIndependentSamplertemperature1e05_20161220212451_0.90min.npz'
+
+  elif context_kind == 'harmonization' and sampling_method == 'yao':
+     # TODO: still ned to run one that uses the same number of model evaluations as NADE.
+     # Harmonization, 100 steps independent Gibbs.
     fpath = '/data/lisatmp4/huangche/new_generated/fromscratch_balanced_by_scaling_init=independent_Gibbsnumsteps100maskerBernoulliInpaintingMaskercontextkindharmonizationscheduleYaoSchedulepmin01pmax09alpha07samplerIndependentSamplertemperature1e05_20161218213502_1.48min.npz'
-  
+
+  else:
+    assert False
+  return fpath
+
+
+def get_npz_data(fpath=None, batch_idx=None):
   data = np.load(fpath)
   pianorolls = data["pianorolls"]
   predictions = data["predictions"]
@@ -92,19 +115,24 @@ def get_npz_data(fpath=None):
   print pianorolls.shape, predictions.shape, masks.shape
   S, B, T, P, I = pianorolls.shape
   
-  # Take the first one in the batch
-  batch_idx = 0
+  if batch_idx is None:
+    return pianorolls, masks, predictions
   rolls = pianorolls[:, batch_idx, :, :, :]
   masks = masks[:, batch_idx, :, :, :]
   predictions = predictions[:, batch_idx, :, :, :]
   return rolls, masks, predictions
 
 
-def get_nade_pianoroll():
-  # Second paper plot.
-  fpath = '/data/lisatmp4/huangche/new_generated/fromscratch_balanced_by_scaling_init=bach_Gibbs-num-steps-0--masker-None--schedule-ConstantSchedule-None---sampler-SequentialSampler-temperature-1e-05--_20161121235937_1.03min.npz'
-  rolls, masks, predictions = get_npz_data(fpath)
-  return rolls[-1]
+def get_gibbs_process(context_kind, sampling_method, batch_idx):
+  fpath = get_fpath(context_kind, sampling_method)  
+  return get_npz_data(fpath, batch_idx)
+
+
+def get_nade_pianorolls(context_kind, batch_idx):
+  fpath = get_fpath(context_kind, 'nade')
+  rolls, masks, predictions = get_npz_data(fpath, batch_idx)
+  step_idx = len(rolls) - 2 
+  return rolls[-1], step_idx
 
 
 # Plotting functions
@@ -252,15 +280,43 @@ def plot_fancy_pianoroll(ax, T, pitch_lb, pitch_ub, roll, proll, prediction, con
   ax.set_title('Step %d' % (step-2))
 
 
-def plot_mask(ax, mask):
+def plot_mask(ax, mask, axes_visible=False):
     """Show instrument by time mask."""
     T, P, I = mask.shape
     #print T, P, I
     mask = np.clip(mask.sum(axis=1), 0, 1)
-    # Instrument by time
+    # Instrument by time.
     ax.imshow(np.flipud(1-mask.T), aspect='equal', cmap='Greys', origin='lower', interpolation='none')
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+    ax.get_xaxis().set_visible(axes_visible)
+    ax.get_yaxis().set_visible(axes_visible)
+
+
+def plot_mask_colored(ax, mask, axes_visible=False):
+    plot_mask(ax, mask, axes_visible)
+    T, P, I = mask.shape
+    colors = CONTEXT_COLORS[::-1]
+    #mask = np.flipud(1-mask.T)
+    mask = mask[:, 0, :]
+    mask = np.fliplr(1 - mask)
+    for i in range(I):
+     for t in np.where(mask[:, i])[0]:
+       ax.add_patch(Rectangle((t-.5, i-.5), 1, 1,
+           facecolor=colors[i], edgecolor='none'))
+
+
+def get_pitch_bounds(rolls, repr_lb=36, repr_ub=88):
+  pitch_lb, pitch_ub = [127, 0]
+  T, P, I = rolls[0].shape
+  assert repr_ub - repr_lb + 1 == P
+  for roll in rolls:
+    roll = roll.sum(axis=-1)
+    pitches = np.where(roll)[1] 
+    pitch_lb = np.minimum(pitch_lb, np.min(pitches))
+    pitch_ub = np.maximum(pitch_ub, np.max(pitches))
+  pitch_lb += repr_lb
+  pitch_ub += repr_lb + 2
+  print pitch_lb, pitch_ub
+  return pitch_lb, pitch_ub
 
 
 def get_process():
@@ -302,32 +358,78 @@ def get_process():
   plt.close()
 
 
-def plot_mask_prediction_realizations():
-  rolls, masks, predictions = get_npz_data()
+def plot_all_mask_prediction_realizations():
+  CONTEXT_KIND = 'transition'
+  #CONTEXT_KIND = 'harmonization'
+  path = get_unique_output_path()
+  #rolls, masks, predictions = get_gibbs_process(CONTEXT_KIND, 'yao')
+  rolls, masks, predictions = get_gibbs_process(CONTEXT_KIND, 'yao', None)
+  nade_pianorolls, nade_step_idx = get_nade_pianorolls(CONTEXT_KIND, None)
+  S, B, T, P, I = rolls.shape
+  print rolls.shape, masks.shape, predictions.shape, nade_pianorolls.shape
+#  for batch_idx in range(B):
+  for batch_idx in [53]:
+  #for batch_idx in range(1):
+    plot_mask_prediction_realizations(
+        rolls[:, batch_idx, :, :, :], masks[:, batch_idx, :, :, :], 
+        predictions[:, batch_idx, :, :, :], nade_pianorolls[batch_idx, :, :, :],
+        nade_step_idx, batch_idx, CONTEXT_KIND, path)
+
+
+def plot_mask_prediction_realizations(rolls, masks, predictions,
+                                      nade_pianoroll, nade_step_idx, 
+                                      batch_idx, context_kind, path):
+  ROW_FOR_TIME = True
   S, T, P, I = rolls.shape
   print 'rolls.shape', rolls.shape
   original = rolls[0]
   context = rolls[1]
   blankouts = original - context
-  plot_steps = [2, 3, 522, S-1]
+  plot_steps = [2, 3, (S-1)/2, S-1]
+  plot_steps = [2, 3, 16+2, S-1]
   is_subplot = True
+
   n_rows = 4
   n_cols = 4
+
+  n_rows = len(plot_steps) + 1
+  n_cols = 3
+  if not ROW_FOR_TIME:
+    temp = n_rows
+    n_rows = n_cols
+    n_cols = temp
   sz_of_subplot = 4
   height = sz_of_subplot * n_rows
   width = sz_of_subplot * n_cols
   figs, axes = plt.subplots(n_rows, n_cols, figsize=(width, height))
+
   pitch_lb=43 #36 #43
   pitch_ub=72 #89 #77
+  
+  pitch_lb, pitch_ub = get_pitch_bounds([original, nade_pianoroll, rolls[-1]])
 
   for i, step in enumerate(plot_steps):
     # plot mask
-    ax = axes[i, 0]
+    if ROW_FOR_TIME:
+      ax = axes[i, 0]
+    else:
+      ax = axes[0, i]
     mask = masks[step]
-    plot_mask(ax, mask) 
+    plot_mask_colored(ax, mask) 
+    if i == 0:
+      ax.set_title('Annealed mask\nfor independent Gibbs\nStep %d' % (step-2))
+      ax.set_ylabel('Instrument')
+    else:
+      ax.set_title('Step %d' % (step-2))
+  
+    if i == len(plot_steps) - 1:
+      ax.set_xlabel('Time')
 
     # plot predictions
-    ax = axes[i, 1]
+    if ROW_FOR_TIME:
+      ax = axes[i, 1]
+    else:
+      ax = axes[0, i]
     roll = rolls[step]
     proll = rolls[step-1]
     prediction = predictions[step] 
@@ -338,31 +440,55 @@ def plot_mask_prediction_realizations():
     plot_pianoroll_with_colored_voices(ax, prediction, imshow=True, plot_boxes=False)
     # with generated context
     plot_pianoroll_with_colored_voices(ax, proll*(1-mask), colors=GENERATED_COLORS)
-
-    #plot_fancy_pianoroll(
-    #    ax, T, pitch_lb, pitch_ub, roll, proll, prediction, context, step, is_subplot, 
-    #    plot_current_step=False)
+    if i == 0:
+      ax.set_title('Independent Gibbs\nStep %d (predictions)' % (step-2))
+    else:
+      ax.set_title('Step %d' % (step-2))
 
     # plot realizations
-    ax = axes[i, 2]
+    if ROW_FOR_TIME: 
+      ax = axes[i, 2]
+    else:
+      ax = axes[2, i]
     #print np.sum(roll)
     plot_pianoroll_with_colored_voices_with_style(ax, roll, pitch_lb, pitch_ub, is_subplot)
-    ax.set_title('Step %d' % (step-2))
+    if i == 0:
+      ax.set_title('Independent Gibbs\nStep %d (sampled realizations)' % (step-2))
+    elif i == len(plot_steps) - 1:
+      ax.set_title('Independent Gibbs\nStep %d' % (step-2))
+    else:
+      ax.set_title('Step %d' % (step-2))
 
   # Shows NADE generated pianoroll.
   # this example was for the transition inpainting.
-  #ax = axes[-2, 3]
-  #nade_pianoroll = get_nade_pianoroll()
-  #plot_pianoroll_with_colored_voices_with_style(ax, nade_pianoroll, pitch_lb, pitch_ub, is_subplot)
-  #ax.set_title('NADE')
+  if ROW_FOR_TIME:
+    ax = axes[-1, 1]
+  else:
+    ax = axes[1, -1]
+  plot_pianoroll_with_colored_voices_with_style(ax, nade_pianoroll, pitch_lb, pitch_ub, is_subplot)
+  #ax.set_title('NADE\nStep %d' % (nade_step_idx - 1))
+  ax.set_title('NADE')
 
   # Showing the original.
-  ax = axes[-1, 3]
+  if ROW_FOR_TIME:
+    ax = axes[-1, 2]
+  else:
+    ax = axes[2, -1]
   plot_pianoroll_with_colored_voices_with_style(ax, original, pitch_lb, pitch_ub, is_subplot)
   ax.set_title('Ground Truth')
+
+  # Remove axis for empty slot.
+  if ROW_FOR_TIME:
+    ax = axes[-1, 0]
+  else:
+    ax = axes[0, -1]
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+  ax.set_frame_on(False)
  
-  path = get_unique_output_path()
-  fname_prefix = 'gibbs_process'
+  fname_prefix = 'gibbs_%s-%d' % (context_kind, batch_idx) 
+  if not ROW_FOR_TIME:
+    fname_prefix += 'column_as_step'
   output_path = os.path.join(path, fname_prefix + '.png') 
   print 'Plotting to', output_path
   plt.savefig(output_path, bbox_inches='tight')
@@ -374,7 +500,7 @@ def main(argv):
   # Paper plot for NADE process.
   # get_process()
 
-  plot_mask_prediction_realizations()
+  plot_all_mask_prediction_realizations()
 
 
 if __name__ == '__main__':
