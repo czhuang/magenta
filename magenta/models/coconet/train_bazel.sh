1e51d38b0bbc8dbc72a86acb933c72560af40dc2
Anna Huang <chengzhiannahuang@gmail.com>
2017-11-13 16:02:38 -0800

    add sketch of bazel train script

---
+++ magenta/models/coconet/train_bazel.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# Assumes that this script is run from within the Coconet root directory.  Change the following line if this is not the case.
+code_dir=$(dirname $0)
+# Change this to dir for saving experiment logs. 
+log_dir="logs"
+# Change this to where data is loaded from.
+#data_dir="$code_dir/testdata"
+data_dir="$code_dir/data"
+dataset=Jsb16thSeparated
+
+# Data preprocessing.
+crop_piece_len=64
+separate_instruments=True
+quantization_level=0.125  # 16th notes
+
+# Hyperparameters.
+maskout_method=orderless
+num_layers=64
+num_filters=128
+batch_size=10
+
+# Run command.
+bazel run -c opt --config=cuda --copt="-mavx" :train \
+  -- \
+  --log_dir=$log_dir \
+  --log_process=True \
+  --data_dir=$data_dir \
+  --dataset=$dataset \
+  --crop_piece_len=$crop_piece_len \
+  --separate_instruments=$separate_instruments \
+  --quantization_level=$quantization_level \
+  --maskout_method=$maskout_method \
+  --num_layers=$num_layers \
+  --num_filters=$num_filters \
+  --use_residual \
+  --batch_size=$batch_size \
